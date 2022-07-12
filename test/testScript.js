aa23526d02ac758672e436f127e0d64389914b01
David Hollinger <david.hollinger123@gmail.com>
2022-07-11 17:33:44 -0700

    Most of landing done, needs moving background

---
+++ test/testScript.js
@@ -0,0 +1,32 @@
+
+
+
+const canvas = document.getElementById("canvas1");
+console.log(canvas);
+const ctx = canvas.getContext('2d');
+
+ctx.canvas.width = window.innerWidth;
+ctx.canvas.height = window.innerHeight;
+console.log(ctx)
+let particleArray;
+
+//constructor of particles
+function Particle(x, y, directionX, directionY, size, color){
+    this.x = x;
+    this.y = y;
+    this.directionX = directionX;
+    this.directionY = directionY;
+    this.size = size;
+    this.color = color;
+}
+// add draw function to particle prototype
+Particle.prototype.draw = function(){
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+}
+
+
+const particle1 = new Particle(100, 100, 1, 1, 100, 'white');
+particle1.draw();
